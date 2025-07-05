e358b4d02e6f8486cb66912663f6fc5bc360dab3
dev-chandan-pandey <iamchandanpandey11@gmail.com>
2025-07-05 22:16:35 +0530

    added

---
+++ l2-advanced-task-manager-with-prioritization/tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+export default {
+  // Configure Tailwind to scan your React components for class names
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
